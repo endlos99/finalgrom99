6e97a8137ca5c3e63b54769da7f057143cd99138
Ralph Benzinger <r@0x01.de>
2020-10-05 13:21:38 +0200

    convert make.py to Python 3

--- ti/make.py
+++ ti/make.py
@@ -4,8 +4,8 @@ import sys
 from subprocess import call
 
 
-xas = ["xas99.py", "-b", "-R", "-I", "/home/ralph/ti99/xdt99/lib/"]
-xga = ["xga99.py"]
+xas = ["/vol/git/xdt99/xas99.py", "-b", "-R", "-I", "/home/ralph/ti99/xdt99/lib/"]
+xga = ["/vol/git/xdt99/xga99.py"]
 
 
 if call(xas + ["menu.a99"], shell=False):
@@ -17,8 +17,8 @@ with open("menu.bin", "rb") as f:
 with open("menu.c", "w") as f:
     f.write("const uint8_t menu[] PROGMEM = {\n");
     for b in data[:-1]:
-        f.write(hex(ord(b)) + ",\n");
-    f.write(hex(ord(data[-1])) + "};\n");
+        f.write(hex(b) + ",\n");
+    f.write(hex(data[-1]) + "};\n");
 
 
 if call(xga + ["grom.gpl"], shell=False):
@@ -30,8 +30,8 @@ with open("grom.gbc", "rb") as f:
 with open("grom.c", "w") as f:
     f.write("const uint8_t grom[] PROGMEM = {\n");
     for b in data[:-1]:
-        f.write(hex(ord(b)) + ",\n");
-    f.write(hex(ord(data[-1])) + "};\n");
+        f.write(hex(b) + ",\n");
+    f.write(hex(data[-1]) + "};\n");
 
 
 if call(xas + ["help.a99"], shell=False):
@@ -43,5 +43,5 @@ with open("help.bin", "rb") as f:
 with open("help.c", "w") as f:
     f.write("const uint8_t help[] PROGMEM = {\n");
     for b in data[:-1]:
-        f.write(hex(ord(b)) + ",\n");
-    f.write(hex(ord(data[-1])) + "};\n");
+        f.write(hex(b) + ",\n");
+    f.write(hex(data[-1]) + "};\n");
